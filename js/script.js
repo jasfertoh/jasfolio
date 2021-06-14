f3288942a07854203222282e1e5730839e78a877
Jasfer <59435921+jasfertoh@users.noreply.github.com>
2021-06-14 21:53:31 +0800

    added more files as well as testimonials and certificates section

--- js/script.js
+++ js/script.js
@@ -80,3 +80,40 @@ function closeAllSelect(elmnt) {
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+  var body = document.body;
+  body.classList.add("loading");
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+  var body = document.body;
+  body.classList.remove("loading")
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
