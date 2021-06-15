bcfc4f27f1df3a77d0acff65e1c0591407bfb329
Jasfer <59435921+jasfertoh@users.noreply.github.com>
2021-06-15 19:05:57 +0800

    Added pcards and more.

--- js/app.js
+++ js/app.js
@@ -10,3 +10,36 @@ $(window).on('load', function() {
         $('.select-selected').css("visibility", "visible");
     });
 });
+
+var videoModalbtn = $(".previewmax");
+var videoModalbtn1 = $(".preview");
+var videoModal = $("#videoModal");
+
+function openVideo(num) {
+  videoModal.css("display", "block");
+  $(".videoModal-body " + num).css("display", "block");
+  $('body').addClass('loading');
+}
+
+function closeVideo() {
+  videoModal.css("display", "none");
+  $('body').removeClass('loading');
+}
+
+function outsideClick(e) {
+  if(e.target == videoModal) {
+    videoModal.style.display = 'none';
+  }
+}
+
+$(videoModalbtn).on('click', function() {
+    // this.append wouldn't work
+    var clickedBtnID = "#" + $(this).attr('id')
+    openVideo(clickedBtnID)
+});
+
+$(videoModalbtn1).on('click', function() {
+    // this.append wouldn't work
+    var clickedBtnID = "#" + $(this).attr('id')
+    openVideo(clickedBtnID)
+});
